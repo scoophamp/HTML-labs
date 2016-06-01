2be0858656203e7142620a038190b6911aa8fe27
scoophamp <hampus_cousin@hotmailcom>
2016-06-01 18:56:47 +0200

    fixed links

--- Laborations/Lab4/scripts/Aquarium.js
+++ Laborations/Lab4/scripts/Aquarium.js
@@ -2,20 +2,21 @@
        draw();
     });
 
-function draw() {
+
   
-    var canvas = document.getElementById('myaq');
-    
-    var ctx = canvas.getContext('2d');
-    ctx.strokeStyle = "black";
-    
-    ctx.strokeRect(20, 20, 150, 100);
-    
-    
-    ctx.fillStyle = "lightblue";
-    
-    ctx.fillRect(20, 20, 150, 100);
+    function draw() {
+        var canvas = document.getElementById('myaq');
+        var context = canvas.getContext('2d');
+        context.strokeStyle = "black";
+        context.strokeRect(20, 20, 150, 100);
+        context.shadowBlur = 25;
+        context.shadowOffsetX = 15;
+        context.shadowOffsetY = 15;
+        context.shadowColor = "black";
+        context.fillStyle = "lightblue";
+        context.fillRect(20, 20, 150, 100);
     
    
-}
+    }
+
 
